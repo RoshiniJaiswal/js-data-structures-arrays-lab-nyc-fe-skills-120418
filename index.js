bfed17f188e735e7479373944164dc05cec6fc93
Roshini Jaiswal <roshini.s.jaiswal@gmail.com>
2019-01-23 02:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution here!
+
+  const drivers = ["Milo", "Otis", "Garfield"]
+ 
+ function destructivelyAppendDriver(name){
+   drivers.push(name)
+ }
+ 
+ function destructivelyPrependDriver(name){
+   drivers.unshift(name)
+ }
+ 
+ function destructivelyRemoveLastDriver(name){
+   drivers.pop(name)
+ }
